6c7be4628a6706c2b73a0e435b3bf05515d1e576
pauloviola16 <pcviola.dev@gmail.com>
2026-04-24 10:29:09 -0300

    study: add Getter e Setters

--- src/main/java/com/example/task_api/dto/TaskRequest.java
+++ src/main/java/com/example/task_api/dto/TaskRequest.java
@@ -1,7 +1,17 @@
 package com.example.task_api.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class TaskRequest {
 
     private String title;
 
+    public TaskRequest(String title) {
+        this.title = title;
+    }
+    public TaskRequest() {
+    }
 }
--- src/main/java/com/example/task_api/dto/TaskResponse.java
+++ src/main/java/com/example/task_api/dto/TaskResponse.java
@@ -1,9 +1,20 @@
 package com.example.task_api.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class TaskResponse {
 
     private Long id;
     private String title;
     private boolean done;
+
+    public TaskResponse(String title) {
+        this.title = title;
+    }
+    public TaskResponse(){
+    }
 
 }
